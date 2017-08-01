3488164b97a7c13c7b48fd1c56febcc03ab73115
klaymanlei <klay.lei@aliyun.com>
2017-08-01 14:39:44 +0800

    拆分文字段前先按空格分割

--- src/main/java/com/klaymanlei/java/toolbox/SentenceAnalyzer.java
+++ src/main/java/com/klaymanlei/java/toolbox/SentenceAnalyzer.java
@@ -12,9 +12,19 @@ public class SentenceAnalyzer {
     (?![\dA-Za-z\u3007\u3400-\u4DB5\u4E00-\u9FCB\uE815-\uE864]|[\uD840-\uD87F][\uDC00-\uDFFF])
      */
     public static final String WORD_REG = "[^\\dA-Za-z\\u3007\\u4E00-\\u9FCB\\uE815-\\uE864]+";
-    public static List<String> pretreat(String str) {
-        return split(cleaning(str));
 
+    /**
+     * 把一句话中非数字字母和汉字的符号替换成空格，之后按空格分成几个短剧，返回每个断句中的文字列表。
+     * 注意：连续的英文和数字算作同一个字
+     */
+    public static List<List<String>> pretreat(String sentence) {
+        String cleaned = cleaning(sentence); // 把非数字字母和汉字的符号替换成空格
+        String[] strs = cleaned.trim().split(" ");
+        List<List<String>> resultSet = new ArrayList<List<String>>();
+        for (String str : strs) {
+            resultSet.add(split(str));
+        }
+        return resultSet;
     }
 
     public static String cleaning(String str) {
@@ -25,38 +35,27 @@ public class SentenceAnalyzer {
         if (str.length() == 0)
             return null;
         List<String> list = new ArrayList<String>();
-        if (str.contains(" ")) {
-            // 字符串包含空格的时候
-            String[] subStrs = str.split(" ");
-            for (String subStr : subStrs) {
-                if (subStr.length() > 0)
-                    list.addAll(split(subStr));
+        int lastWordType = 1; // 上一个字符的类型，0：数字或字母；1：其他
+        StringBuffer buf = new StringBuffer();
+        for (Character c : str.toCharArray()) {
+            if (Character.isDigit(c) || Character.isUpperCase(c) || Character.isLowerCase(c)) {
+                // 连续的字母和数字算作一个单字
+                lastWordType = 0;
+                buf.append(c);
             }
-            return list;
-        } else {
-            int i = 0;
-            int lastWordType = 1; // 上一个字符的类型，0：数字或字母；1：其他
-            StringBuffer buf = new StringBuffer();
-            for (Character c : str.toCharArray()) {
-                if (Character.isDigit(c) || Character.isUpperCase(c) || Character.isLowerCase(c)) {
-                    // 连续的字母和数字算作一个单字
-                    lastWordType = 0;
-                    buf.append(c);
-                }
-                else {
-                    // 每个汉字作为一个单字
-                    if (lastWordType == 0) list.add(buf.toString().toLowerCase());
-                    buf.setLength(0);
-                    list.add(String.valueOf(c));
-                    lastWordType = 1;
-                }
-            }
-            if (buf.length() > 0) {
-                list.add(buf.toString().toLowerCase());
+            else {
+                // 每个汉字作为一个单字
+                if (lastWordType == 0) list.add(buf.toString().toLowerCase());
                 buf.setLength(0);
+                list.add(String.valueOf(c));
+                lastWordType = 1;
             }
-            return list;
         }
+        if (buf.length() > 0) {
+            list.add(buf.toString().toLowerCase());
+            buf.setLength(0);
+        }
+        return list;
     }
 
     // 把一句话按照每2个字、3个字、4个字、5个字、6个字拆成小文字段，并返回所有出现过的文字段
@@ -76,10 +75,15 @@ public class SentenceAnalyzer {
     }
 
     public static void main(String[] args) {
-        List<String> chars = pretreat("《中国新闻》2013年世界游泳锦标赛 孙杨卫冕800米自由泳金牌");
-        Set<String> words = toElements(chars);
-        System.out.println(words.size());
-        for (String w: words) {
+        List<List<String>> chars = pretreat("《中国新闻》2013年世界游泳锦标赛 孙杨卫冕800米自由泳金牌");
+        Set<String> resultSet = new HashSet<String>();
+        for (List<String> list : chars) {
+            //System.out.println(list);
+            Set<String> words = toElements(list);
+            resultSet.addAll(words);
+        }
+        System.out.println(resultSet.size());
+        for (String w: resultSet) {
             System.out.println(w);
         }
     }
