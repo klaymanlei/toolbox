f40b8127abda1a43e71d4ee4d74bea22aa33f252
klaymanlei <klay.lei@aliyun.com>
2017-08-02 19:27:48 +0800

    从标题中删掉新浪视频字样，并在替换符号时保留减号

--- src/main/java/com/klaymanlei/java/toolbox/SentenceOper.java
+++ src/main/java/com/klaymanlei/java/toolbox/SentenceOper.java
@@ -12,7 +12,7 @@ public class SentenceOper {
     3、豪华规格——针对更多的汉字（支持CJK ExtB、C、……，共计75000多汉字）
     (?![\dA-Za-z\u3007\u3400-\u4DB5\u4E00-\u9FCB\uE815-\uE864]|[\uD840-\uD87F][\uDC00-\uDFFF])
      */
-    public static final String WORD_REG = "[^\\dA-Za-z\\u3007\\u4E00-\\u9FCB\\uE815-\\uE864]+";
+    public static final String WORD_REG = "[^\\dA-Za-z\\u3007\\u4E00-\\u9FCB\\uE815-\\uE864\\-]+";
 
     /**
      * 把一句话中非数字字母和汉字的符号替换成空格，之后按空格分成几个短剧，返回每个断句中的文字列表。
@@ -20,7 +20,7 @@ public class SentenceOper {
      */
     public static List<List<String>> pretreat(String sentence) {
         String cleaned = cleaning(sentence); // 把非数字字母和汉字的符号替换成空格
-        String[] strs = cleaned.trim().split(" ");
+        String[] strs = cleaned.replaceAll("新浪视频", "").trim().split(" ");
         List<List<String>> resultSet = new ArrayList<List<String>>();
         for (String str : strs) {
             resultSet.add(split(str));
@@ -39,7 +39,7 @@ public class SentenceOper {
         int lastWordType = 1; // 上一个字符的类型，0：数字或字母；1：其他
         StringBuffer buf = new StringBuffer();
         for (Character c : str.toCharArray()) {
-            if (Character.isDigit(c) || Character.isUpperCase(c) || Character.isLowerCase(c)) {
+            if (Character.isDigit(c) || Character.isUpperCase(c) || Character.isLowerCase(c) || c == '-' || c == '_') {
                 // 连续的字母和数字算作一个单字
                 lastWordType = 0;
                 buf.append(c);
