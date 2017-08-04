bf53b59fc3405c5420400e5476dd62d175d26b22
klaymanlei <klay.lei@aliyun.com>
2017-08-04 12:40:48 +0800

    把静态方法都改为实例方法

--- src/main/java/com/klaymanlei/java/toolbox/SentenceAnalyzer.java
+++ src/main/java/com/klaymanlei/java/toolbox/SentenceAnalyzer.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class SentenceAnalyzer {
 
     // 把一句话按照每2个字、3个字、4个字、5个字、6个字拆成小文字段，并返回所有出现过的文字段
-    public static Set<String> toElements(List<String> wordList) {
+    public Set<String> toElements(List<String> wordList) {
         Set<String> resultSet = new HashSet<String>();
         if (wordList == null)
             return resultSet;
@@ -27,7 +27,7 @@ public class SentenceAnalyzer {
      * 结果中有互相包含的文字段，且两个文字段的VV相差很小的时候，取文字最长的一段
      * @param resultMap
      */
-    public static void resultCleaning(Map<String, Long> resultMap) {
+    public void resultCleaning(Map<String, Long> resultMap) {
         Set<String> delWord = new HashSet<String>();
         int no = 1;
         for (String word : resultMap.keySet()) {
@@ -38,7 +38,7 @@ public class SentenceAnalyzer {
                 if (otherWord.contains(word) && !otherWord.equals(word)) {
                     long wordVV = resultMap.get(word);
                     long otherVV = resultMap.get(otherWord);
-                    if (((double)otherVV / wordVV) > (95d / 100))
+                    if (((double)otherVV / wordVV) > (90d / 100))
                         delWord.add(word);
                 }
             }
@@ -49,9 +49,8 @@ public class SentenceAnalyzer {
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public TreeMap<Long, Set<String>> analyze(String path) throws IOException {
         // 读取title的VV数据
-        String path = "/home/leidayu/dev/sina/zmodem/videotitleVV.out";
         Map<String, Long> data = FileOper.readData(path);
 
         // 计算文字段的VV
@@ -86,7 +85,7 @@ public class SentenceAnalyzer {
             if (set == null) {
                 set = new HashSet<String>();
                 tmap.put(vv, set);
-                if (tmap.size() > 1000)
+                if (tmap.size() > 5000)
                     tmap.remove(tmap.firstKey());
             }
             set.add(word);
@@ -94,8 +93,8 @@ public class SentenceAnalyzer {
 
         // 清理包含在其他文字段中的段
         resultMap.clear();
-        for(Long vv : tmap.keySet()) {
-            for(String word : tmap.get(vv)) {
+        for (Long vv : tmap.keySet()) {
+            for (String word : tmap.get(vv)) {
                 resultMap.put(word, vv);
             }
         }
@@ -109,12 +108,17 @@ public class SentenceAnalyzer {
             if (set == null) {
                 set = new HashSet<String>();
                 tmap.put(vv, set);
-                if (tmap.size() > 1000)
+                if (tmap.size() > 5000)
                     tmap.remove(tmap.firstKey());
             }
             set.add(word);
         }
+        return tmap;
+    }
 
+    public static void main(String[] args) throws IOException {
+        String path = "/home/leidayu/dev/sina/zmodem/videotitle_sub.out";
+        TreeMap<Long, Set<String>> tmap = new SentenceAnalyzer().analyze(path);
         // 打印
         while(tmap.size() > 0) {
             System.out.println(tmap.lastKey() + ": " + tmap.remove(tmap.lastKey()));
