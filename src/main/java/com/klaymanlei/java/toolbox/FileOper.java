121b9045aa7255e703b203691b92de34a5c347a3
klaymanlei <klay.lei@aliyun.com>
2017-08-16 17:08:53 +0800

    增加append方法

--- src/main/java/com/klaymanlei/java/toolbox/FileOper.java
+++ src/main/java/com/klaymanlei/java/toolbox/FileOper.java
@@ -4,14 +4,16 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class FileOper {
 
     public static void writeData(List<String> lines, String path) throws IOException {
         FileUtils.writeLines(new File(path), lines);
+    }
+
+    public static void appendData(List<String> lines, String path) throws IOException {
+        FileUtils.writeLines(new File(path), lines, true);
     }
 
     public static List<String> readData(String path) throws IOException {
